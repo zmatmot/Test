a27cde2b707f53fa5a63f33b760e2e939c042889
zmatmot <163745716+zmatmot@users.noreply.github.com>
2024-03-18 01:29:05 +0700

    Add files via upload

---
+++ 1/Todolists/apis/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Todolist
+
+class TodolistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todolist
+        fields = '__all__'
